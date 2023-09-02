e3b5ff7c9b0b59852179100a8706b161a5d3919b
Fırat Yesilgöz <56675568+firatyesilgoz@users.noreply.github.com>
2023-09-02 03:21:27 +0300

    contentcard

---
+++ src/app/modules/main/components/contentcard/contentcard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-contentcard',
+  templateUrl: './contentcard.component.html',
+  styleUrls: ['./contentcard.component.scss']
+})
+export class ContentcardComponent {
+
+}
