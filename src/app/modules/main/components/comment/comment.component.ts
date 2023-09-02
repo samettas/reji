1d64b87a54da9c9f805d87433a370a0a73af01e4
Fırat Yesilgöz <56675568+firatyesilgoz@users.noreply.github.com>
2023-09-03 02:57:59 +0300

    comment

---
+++ src/app/modules/main/components/comment/comment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-comment',
+  templateUrl: './comment.component.html',
+  styleUrls: ['./comment.component.scss']
+})
+export class CommentComponent {
+  
+
+}
