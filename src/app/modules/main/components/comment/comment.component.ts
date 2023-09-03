11ea545016259097fcd4df96a997fdea82137dca
Fırat Yesilgöz <56675568+firatyesilgoz@users.noreply.github.com>
2023-09-04 01:14:06 +0300

    tekrardan düzenleyip attım

--- src/app/modules/main/components/comment/comment.component.ts
+++ src/app/modules/main/components/comment/comment.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'main-comment',
+  selector: 'comment-comment',
   templateUrl: './comment.component.html',
   styleUrls: ['./comment.component.scss']
 })
 export class CommentComponent {
-  
+  commentItems = [
+    {
+      name: 'Nihat Zaman',
+      comment: ' Deneme Deneme Deneme Deneme Deneme Deneme Deneme Deneme Deneme Deneme Deneme Deneme',
+      date: 'March 30, 2021'
+    },
+    
+  ];
 
-}
+} 
