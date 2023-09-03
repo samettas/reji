cd7764f5b77ca3f2dd09b6ad1c0988c3e9444892
Fırat Yesilgöz <56675568+firatyesilgoz@users.noreply.github.com>
2023-09-04 01:50:44 +0300

    movieinfo

--- src/app/modules/main/components/movieinfo/movieinfo.component.ts
+++ src/app/modules/main/components/movieinfo/movieinfo.component.ts
@@ -11,11 +11,13 @@ export class MovieinfoComponent {
 
   movies: Movie[] = [
     {
+      
       "id": 1,
       "title": "Greenland",
       "overview": "BİLGİ BİLGİ BİLGİ BİLGİ",
       "release_date": "2010-07-16"
   }
+  
   ];
   
 
