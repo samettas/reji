be26fc71bfb6d517b09f7cd4bb016a368f757f9d
Fırat Yesilgöz <56675568+firatyesilgoz@users.noreply.github.com>
2023-09-04 02:01:52 +0300

    rootpagedüzenleme

--- src/app/modules/main/components/header/header.component.ts
+++ src/app/modules/main/components/header/header.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  HeaderItems = {
+    navigationLeft: [
+      { label: 'Filmler', link: '#' },
+      { label: 'Vizyondaki Filmler', link: '#' },
+      { label: 'Pek Yakında', link: '#' },
+    ],
+    navigationRight: [
+      { label: 'Diziler', link: '#' },
+      { label: 'TV Dizileri', link: '#' },
+      { label: 'Platform Dizileri', link: '#' },
+    ],
+    buttonItems: [{ label: 'Giriş Yap / Kayıt Ol', link: '/auth/' }],
+  };
+}
 
 }
