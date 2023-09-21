2191be6becf7d31f4077d50ab89ba65dc7d5ecf3
Samet Taş <bmsamettas@gmail.com>
2023-09-21 16:23:43 +0300

    mini carousels regulation

--- src/app/modules/main/models/sidebar.model.ts
+++ src/app/modules/main/models/sidebar.model.ts
@@ -1,9 +1,28 @@
-export class SidebarMovie{
-    name?:string;
-    imageUrl?:string;
+export class SidebarModel {
+
+    actorIDs?:ActorData[];
+    description?:string;
+    duration?: string;
+    images?:ImageData[];
+    movieName?:string;
+    releaseDate?:string;
+    thumbnail?:string;
+    trailers?:string[];
+    score?:number;
+    movieID?:string;
+}
+
+export class ImageData{
+    src?:string;
+}
+export class ActorData{
+    label?:string;
 }
 
 export class CommentItem{
+    uID?:string;
     comment?:string;
-    imageUrl?:string;
+    commentDate?:string;
+    movieID?:string;
+    score?:number;
 }
---
+++ src/app/modules/main/services/sidebar.service.ts
@@ -0,0 +1,22 @@
+ import { Injectable, inject } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { SidebarModel } from '../models/sidebar.model';
+import { collection, doc, getDocs } from 'firebase/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidebarService {
+
+  constructor(private fs: Firestore = inject(Firestore)) { }
+
+  async getSidebar() : Promise<SidebarModel[]>{
+    const querySnapshot = await getDocs(collection(this.fs, "movies"));
+    const movieItems:SidebarModel[] = [];
+    querySnapshot.forEach((doc) => {
+      movieItems.push(doc.data() as SidebarModel);
+      console.log(doc.id, " => ", doc.data());
+    });
+    return movieItems;
+  }
+}
