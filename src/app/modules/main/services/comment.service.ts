2bf67a0d27e18bd3f580d1f023404a9686d38a21
Samet Taş <bmsamettas@gmail.com>
2023-09-23 15:55:22 +0300

    latest regulations

--- src/app/modules/main/services/comment.service.ts
+++ src/app/modules/main/services/comment.service.ts
@@ -1,9 +1,4 @@
 import { Injectable, inject } from '@angular/core';
-<<<<<<< HEAD
-import { CommentModel } from '../models/comment.model';
-import { Firestore, doc, getDoc, setDoc ,collection, query, where, limit, collectionSnapshots, getDocs} from '@angular/fire/firestore';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
-=======
 import { Auth } from '@angular/fire/auth';
 import {
   Firestore,
@@ -16,14 +11,12 @@ import {
   setDoc,
   where,
 } from '@angular/fire/firestore';
->>>>>>> 0d13017c6d0b0e516796492349ea12005ff6efc0
+import { CommentModel } from '../models/comment.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CommentService {
-<<<<<<< HEAD
-  isError: boolean = false;
   async getComment() : Promise<CommentModel[]>{
     const querySnapshot = await getDocs(collection(this.fs, "comments"));
     const commentItem:CommentModel[] = [];
@@ -33,11 +26,6 @@ export class CommentService {
     });
     return commentItem;
   }
-  
-  
-  Form = this.fb.group({
-    commentt: new FormControl('', Validators.required),
-=======
 
   constructor(
     private fs: Firestore = inject(Firestore),
@@ -53,7 +41,6 @@ export class CommentService {
     const querySnapshot = await getDocs(q);
 
     return querySnapshot.docs.map((doc) => doc.data());
->>>>>>> 0d13017c6d0b0e516796492349ea12005ff6efc0
     
   }
   async createComment(comment: any) {
